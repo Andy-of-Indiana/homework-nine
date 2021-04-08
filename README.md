e996dfac29e131042b5330942284715e142665d8
Andrew Choi <ac58@iu.edu>
2021-04-07 22:54:37 -0400

    lab11

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # homework-nine
- 
+
+git for homework-nine
+This is the URL for web 4
+https://in-info-web4.informatics.iupui.edu/~ac58/homework-nine
---
+++ app/app.js
@@ -0,0 +1,246 @@
+var homePage = `
+<div class="homePage">
+<section class="homeBG">
+  <nav>
+    <div class="lineWhite">
+      <div class="logo">
+        <a href="index.html">
+          <img
+            src="assets/images/logo-white.png"
+            style="width: 130px"
+          />
+        </a>
+      </div>
+      <div class="linksWhite">
+        <a href="index.html">home</a>
+        <p>|</p>
+        <a href="about.html">about</a>
+        <p>|</p>
+        <a href="tours.html">tours</a>
+        <p>|</p>
+        <a href="specialOffer.html">special offer</a>
+        <p>|</p>
+        <a href="blog.html">blog</a>
+        <p>|</p>
+        <a href="contact.html">contact</a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="homeTitle">
+    <h1>travel-fly</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat
+      aliquid minus nemo sed est.
+    </p>
+    <div class="buttonSquare">
+      <a href="tours.html">READ MORE</a>
+    </div>
+  </div>
+</section>
+
+<section class="homeBookingLinks">
+  <div class="homeTour img1">
+    <h1>ISRAEL</h1>
+    <h2>from $1000</h2>
+    <div class="circleButton">
+      <a href="toursIsrael.html">LEARN MORE</a>
+    </div>
+  </div>
+  <div class="homeTour img2">
+    <h1>U.S.A</h1>
+    <h2>from $1500</h2>
+    <div class="circleButton">
+      <a href="toursUSA.html">LEARN MORE</a>
+    </div>
+  </div>
+  <div class="homeTour img3">
+    <h1>AUSTRAILIA</h1>
+    <h2>from $1800</h2>
+    <div class="circleButton">
+      <a href="toursAustrailia.html">LEARN MORE</a>
+    </div>
+  </div>
+</section>
+
+<section class="homeContent">
+  <div class="innerBinding">
+    <div class="bookingForm">
+      <h1>Booking form</h1>
+      <div class="bookingFormInfo">
+        <div class="bookingFormInfoRow">
+          <div class="textBoxSmall">
+            <p>name...</p>
+          </div>
+          <div class="textBoxLarge">
+            <p>email...</p>
+          </div>
+        </div>
+        <div class="bookingFormInfoRow">
+          <div class="textBoxSmall">
+            <p>country...</p>
+          </div>
+          <div class="textBoxLarge">
+            <p>hotel...</p>
+          </div>
+        </div>
+      </div>
+      <div class="bookingFormCheck">
+        <div class="bookingFormCheckRow">
+          <div class="textBoxOuter">
+            <p>Check-in</p>
+          </div>
+          <div class="textBoxCheck">
+            <p>05/10/2020</p>
+            <div class="calender"></div>
+          </div>
+        </div>
+        <div class="bookingFormCheckRow">
+          <div class="textBoxOuter">
+            <p>Check-out</p>
+          </div>
+          <div class="textBoxCheck">
+            <p>05/20/2020</p>
+            <div class="calender"></div>
+          </div>
+        </div>
+      </div>
+      <div class="bookingFormQuanity">
+        <div class="bookingFormQuanityCol">
+          <div class="textBoxOuterTwo">
+            <p>Adult</p>
+          </div>
+        </div>
+        <div class="bookingFormQuanityCol">
+          <div class="textBoxQuanity">
+            <p>2</p>
+            <div class="dropDown"></div>
+          </div>
+        </div>
+        <div class="bookingFormQuanityCol">
+          <div class="textBoxOuterTwo">
+            <p>Children</p>
+          </div>
+        </div>
+        <div class="bookingFormQuanityCol">
+          <div class="textBoxQuanity">
+            <p>0</p>
+            <div class="dropDown"></div>
+          </div>
+        </div>
+        <div class="bookingFormQuanityCol">
+          <div class="textBoxOuterTwo">
+            <p>Rooms</p>
+          </div>
+        </div>
+        <div class="bookingFormQuanityCol">
+          <div class="textBoxQuanity">
+            <p>1</p>
+            <div class="dropDown"></div>
+          </div>
+        </div>
+      </div>
+      <div class="bookingFormMessage">
+        <p>message...</p>
+      </div>
+    </div>
+
+    <div class="buttonLargeCircle">
+      <a href="#">SUBMIT</a>
+    </div>
+  </div>
+</section>
+</div>
+`;
+var aboutPage = `
+<div class="aboutPage">
+    <nav>
+        <div class="lineBlack">
+            <div class="logo">
+                <a href="index.html">
+                <img src="assets/images/logo-black.png" style="width: 130px;">
+                </a>
+            </div>
+            <div class="linksBlack">
+                <a id="homePage" href="#">home</a>
+                <p>|</p>
+                <a id="aboutPage" href="#">about</a>
+                <p>|</p>
+                <a href="tours.html">tours</a>
+                <p>|</p>
+                <a href="tours.html">tours</a>
+                <p>|</p>
+                <a href="specialOffer.html">special offer</a>
+                <p>|</p>
+                <a href="blog.html">blog</a>
+                <p>|</p>
+                <a href="contact.html">contact</a>
+            </div>
+        </div>
+    </nav>
+
+<section class="aboutContent">
+    <div class="outerBinding">
+        <div class="about">
+            <div class="aboutTitle">
+                <h1>ABOUT:</h1>
+                <div class= "titleLine"></div>
+            </div>
+            <div class="aboutWords">
+                <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+                </p>
+            </div>
+        </div>
+
+        <div class="clientQuotes">
+            <div class="clientQuotesTitle">
+                <h2>CLIENT QUOTES:</h2>
+                <div class= "titleLine"></div>
+            </div>
+            <div class="clientQuotesContent">
+                <div class="clientQuotesBox">
+                    <div class="clientImage one"></div>
+                    <div class="client">
+                        <div class="clientWords">
+                            <p>
+                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+                            </p>
+                        </div>
+                        <div class="clientName">
+                            <p>- Miranda Brown</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="clientQuotesBox">
+                    <div class="clientImage two"></div>
+                    <div class="client">
+                        <div class="clientWords">
+                            <p>
+                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+                            </p>
+                        </div>
+                        <div class="clientName">
+                            <p>- Johnathan wes</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</div>
+`;
+
+function init() {
+    $("nav a").click(function (e) {
+        let btnID = this.id;
+        let contentID = btnID;
+
+        $("#app").html(eval(contentID));
+    });
+}
+
+$(document).ready(function () {
+    init();
+});
